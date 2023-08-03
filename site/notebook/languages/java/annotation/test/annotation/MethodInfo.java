01fff914bf12e9962604dd79ab57d14d781d7b97
KrLite <68179735+KrLite@users.noreply.github.com>
2023-08-03 13:16:38 +0800

    fix

---
+++ site/notebook/languages/java/annotation/test/annotation/MethodInfo.java
@@ -0,0 +1,17 @@
+package test.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodInfo {
+	String id();
+	String name() default "";
+	String author() default "N/A";
+	String date();
+	int revision() default 1;
+	String comment() default "";
+}
