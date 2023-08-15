9824ee8dada30746a50044636a077a0a8f8a5fae
KrLite <68179735+KrLite@users.noreply.github.com>
2023-08-16 00:23:39 +0800

    澤野弘之！

--- docs/javascripts/music_player.js
+++ docs/javascripts/music_player.js
@@ -1,11 +1,26 @@
 var musicPlayers = document.querySelectorAll(".music-player");
 
-for (let i = 0; i < musicPlayers.length; i++) {
-  const musicPlayer = musicPlayers[i];
+Array.from(musicPlayers).forEach(function (musicPlayer) {
   const music = musicPlayer.querySelector("audio");
   const playBtn = musicPlayer.querySelector(".music-player-btn__play");
   const seekBar = musicPlayer.querySelector(".music-player__seek-bar");
-  const time = document.querySelector(".music-player__time");
+  const volumeBar = musicPlayer.querySelector(".music-player__volume-bar");
+  const time = musicPlayer.querySelector(".music-player__time-text");
+  const volume = musicPlayer.querySelector(".music-player__volume");
+  const disk = musicPlayer.querySelector(".music-player__disk");
+  const shifts = musicPlayer.querySelectorAll(".music-player__shift");
+
+  var diskRotation = 0;
+  var diskRotationDelta = 0;
+  var diskRotationDeltaTarget = 0;
+  var diskRotationSpeed = 0.1;
+
+  setInterval(() => {
+    diskRotationDelta += (diskRotationDeltaTarget - diskRotationDelta) * 0.04;
+	diskRotation += diskRotationDelta * diskRotationSpeed;
+	diskRotation = diskRotation % 360;
+    disk.style.transform = `rotate(${diskRotation}deg)`;
+  }, 1000 / 60);
 
   const formatTime = (time) => {
     let min = Math.floor(time / 60);
@@ -20,25 +35,41 @@ for (let i = 0; i < musicPlayers.length; i++) {
   };
 
   seekBar.value = 0;
+  volumeBar.value = 87;
+  volumeBar.max = 100;
 
   music.addEventListener("canplaythrough", () => {
     if (music.paused) {
       playBtn.classList.add("pause");
+	  diskRotationDeltaTarget = 0;
     } else {
       playBtn.classList.remove("pause");
+	  diskRotationDeltaTarget = 1;
     }
     seekBar.max = music.duration;
+    music.volume = volumeBar.value / 100;
     time.innerHTML = `<code>${formatTime(music.duration)}</code>`;
   });
 
+  music.addEventListener("play", () => {
+    disk.classList.add("playing");
+	diskRotationDeltaTarget = 1;
+  });
+
+  music.addEventListener("pause", () => {
+    disk.classList.remove("playing");
+	diskRotationDeltaTarget = 0;
+  });
+
   playBtn.addEventListener("click", () => {
-	if (music.ended) {
-		music.currentTime = 0;
-	}
+    if (music.ended) {
+      music.currentTime = 0;
+    }
 
     if (music.paused || music.ended) {
       music.play();
       playBtn.classList.remove("pause");
+	  diskRotationDelta
     } else {
       music.pause();
       playBtn.classList.add("pause");
@@ -54,6 +85,10 @@ for (let i = 0; i < musicPlayers.length; i++) {
     }
   });
 
+  setInterval(() => {
+    time.style.opacity = 1 - parseFloat(getComputedStyle(volumeBar).opacity);
+  }, 100);
+
   seekBar.addEventListener("input", () => {
     time.innerHTML = `<code>
 	${formatTime(seekBar.value)} / ${formatTime(music.duration)}
@@ -66,13 +101,39 @@ for (let i = 0; i < musicPlayers.length; i++) {
 
   seekBar.addEventListener("mousedown", () => {
     seekBar.classList.add("active");
+    time.classList.add("opaque");
   });
 
   seekBar.addEventListener("mouseup", () => {
     seekBar.classList.remove("active");
+    time.classList.remove("opaque");
   });
 
   music.addEventListener("ended", () => {
-	playBtn.classList.add("pause");
+    playBtn.classList.add("pause");
   });
-}
+
+  volumeBar.addEventListener("input", () => {
+    music.volume = volumeBar.value / 100;
+  });
+
+  Array.from(shifts).forEach(function (shift) {
+    shift.addEventListener("mousemove", function (event) {
+      var rect = shift.getBoundingClientRect();
+      var mouseX = event.clientX - rect.left;
+      var mouseY = event.clientY - rect.top;
+
+      var width = shift.offsetWidth;
+      var height = shift.offsetHeight;
+      var shiftX =
+        Math.max(-1, Math.min(1, (mouseX - width / 2) / width)) * 0.3;
+      var shiftY =
+        Math.max(-1, Math.min(1, (mouseY - height / 2) / height)) * 0.3;
+
+      shift.style.setProperty("--shift-x", `${shiftX}em`);
+      shift.style.setProperty("--shift-y", `${shiftY}em`);
+      shift.style.setProperty("--scale-x", `${1 + Math.abs(shiftX) / 5}`);
+      shift.style.setProperty("--scale-y", `${1 + Math.abs(shiftY) / 5}`);
+    });
+  });
+});
--- docs/test.md
+++ docs/test.md
@@ -4,16 +4,18 @@ hide:
   - toc
 ---
 
+# 试试这个！
+
 <section class="music-player">
-<audio src="https://github.com/KrLite/BrokenThoughts/raw/main/docs/assets/music/avid/6d9bfe7c4a2e7decbc9e171779b91c5f.mp3" id="audio"></audio>
-<div class="music-player__container music-player__padding-around" style="--music-fg-color--light: rgba(128, 23, 66, 1);--music-fg-color--dark: rgba(255, 108, 197, 1);--music-bg-color--light: rgba(128, 23, 66, 0.1);--music-bg-color--dark: rgba(255, 108, 197, 0.1);">
-  <div class="music-player__disk" style="--music-disk-image: url('https://github.com/KrLite/BrokenThoughts/blob/main/docs/assets/music/avid/cover.png?raw=true');"></div>
-  <div class="music-player__inner-container music-player__padding-sides">
-    <div class="music-player__container">
-      <h1>Avid</h1>
-	  <span class="music-player__artist">
-	    <h1>澤野弘之</h1>
-	  </span>
+<audio src="../assets/music/avid/6d9bfe7c4a2e7decbc9e171779b91c5f.mp3" id="audio"></audio>
+<div class="music-player__container-main music-player__padding-around" style="--music-fg-color--light: rgba(128, 23, 66, 1);--music-fg-color--dark: rgba(255, 108, 197, 1);--music-bg-color--light: rgba(128, 23, 66, 0.1);--music-bg-color--dark: rgba(255, 108, 197, 0.1);">
+  <div class="music-player__disk" style="--music-disk-image: url('../assets/music/avid/cover.png');"></div>
+  <div class="music-player__inner-container-main music-player__padding-sides">
+    <div class="music-player__title-container">
+	  <h1>
+	    <span class="music-player__title">Avid</span>
+	    <span class="music-player__artist">澤野弘之</span>
+	  </h1>
 	  <div class="music-player__controls">
         <button class="music-player-btn__play pause">
           <span></span>
@@ -22,9 +24,14 @@ hide:
       </div>
 	</div>
 	<div class="music-player__container">
-	  <span class="music-player__time"></span>
+	  <span class="music-player__time">
+	    <span class="music-player__volume music-player__shift">
+		  <input type="range" value="0" class="music-player__volume-bar">
+		</span>
+	    <span class="music-player__time-text"></span>
+	  </span>
 	  <div class="music-player__inner-container">
-	    <div class="music-player__slider">
+	    <div class="music-player__slider music-player__shift">
           <input type="range" value="0" class="music-player__seek-bar">
         </div>
 	  </div>
